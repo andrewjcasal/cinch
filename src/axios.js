df8c61d662ba35c3754576cfc925a3196a62c54c
Michael Klein <miketheklein@gmail.com>
2018-03-06 12:42:25 -0800

    getting response back from call to server for organizations page

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const instance = axios.create({})
+const instance = axios.create({
+  baseURL: 'http://66f969d5.ngrok.io'
+})
 
 instance.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
