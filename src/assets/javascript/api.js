c190f165e2b6ace55232091cf51518ed05cda9c0
Michael Klein <miketheklein@gmail.com>
2018-03-20 14:49:37 -0700

    adding in baseUrl to api call

--- src/assets/javascript/api.js
+++ src/assets/javascript/api.js
@@ -12,26 +12,27 @@ const options = {
 const url = {
   dev: 'http://ec2-34-212-245-30.us-west-2.compute.amazonaws.com/api/v1'
 }
+const baseUrl = url.dev
 
 export default {
   getAllOrganizations: function(url) {
-    return axios.get(url.dev + '/organizations', options) 
+    return axios.get(baseUrl + '/organizations', options) 
   },
 
   getAllSports: function(url) {
-    return axios.get(url.dev + '/ui/sports', options)
+    return axios.get(baseUrl + '/ui/sports', options)
   },
   getAllAffiliations: function(url) {
-    return axios.get(url.dev + '/ui/governing-bodies', options)
+    return axios.get(baseUrl + '/ui/governing-bodies', options)
   },
   getAllProgramTypes: function(url) {
-    return axios.get(url.dev + '/ui/program-types', options) /*? */
+    return axios.get(baseUrl + '/ui/program-types', options) /*? */
   },
   getAllStates: function(url) {
-    return axios.get(url.dev + '/ui/states', options) /*? */
+    return axios.get(baseUrl + '/ui/states', options) /*? */
   },
   getAllRelationshipTypes: function(url) {
-    return axios.get(url.dev + '/ui/relationship-types', options)
+    return axios.get(baseUrl + '/ui/relationship-types', options)
   }
 };
 
