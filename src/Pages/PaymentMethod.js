3d09adef39b93053188f3c3241cd1d55e8821895
Michael Klein <miketheklein@gmail.com>
2018-04-03 09:55:44 -0700

    payment method WIP

--- src/Pages/PaymentMethod.js
+++ src/Pages/PaymentMethod.js
@@ -17,39 +17,75 @@ const PaymentMethod = () => (
       </Col>
       <Col md={9}>
         <SectionWithHeading heading="Payment Method">
-          <ContentContainer>
-            <Col md={8}>
-              <Row>
-                <Col md={6}>
-                  <p class="subheading">Cardholder name</p>
-                  <input type="text" placeholder="Enter Name" />
-                </Col>
-                <Col md={6}>
-                  <p class="subheading">Card number</p>
-                  <input type="text" placeholder="Enter Card Number" />
-                </Col>
-              </Row>
-              <Row>
-                <Col md={4}>
-                  <p class="subheading">Expire Date</p>
-                  <Dropdown options={dropdownOptions.Months} title="MM" />
-                </Col>
-                <Col md={4}>
-                  <p class="subheading">&nbsp;</p>
-                  <Dropdown options={dropdownOptions.Years} title="YYYY" />
-                </Col>
-                <Col md={4}>
-                  <p class="subheading">CVV</p>
-                  <input type="text" placeholder="Enter Card Number" />
-                </Col>
-              </Row>
-              <Row>
-                <Col md={12}><FooterButtons centered save="black" /></Col>
-              </Row>
-            </Col>
-            <Col md={4}></Col>
-            <Clearfix />
-          </ContentContainer>
+          <Col md={8}>
+            <ContentContainer>
+              <Col md={12}>
+                  <Row>
+                    <Col md={6}>
+                      <p class="subheading">Cardholder name</p>
+                      <input type="text" placeholder="Enter Name" />
+                    </Col>
+                    <Col md={6}>
+                      <p class="subheading">Card number</p>
+                      <input type="text" placeholder="Enter Card Number" />
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col md={4}>
+                      <p class="subheading">Expire Date</p>
+                      <Dropdown options={dropdownOptions.Months} title="MM" />
+                    </Col>
+                    <Col md={4}>
+                      <p class="subheading">&nbsp;</p>
+                      <Dropdown options={dropdownOptions.Years} title="YYYY" />
+                    </Col>
+                    <Col md={4}>
+                      <p class="subheading">CVV</p>
+                      <input type="text" placeholder="Enter Card Number" />
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col md={12}><FooterButtons centered save="black" /></Col>
+                  </Row>
+              </Col>
+            </ContentContainer>
+          </Col>
+          <Col md={4}>
+            <ContentContainer>
+              <Col md={12}>
+                  <Col md={12}>
+                    <Row>
+                      <Col>
+                        <Row>
+                          <a href="#" className="tick selected"></a>
+                          <img src="../../assets/images/visa.png" />
+                          <p>*5495 (Default)</p>
+                        </Row>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col>
+                        <Row>
+                          <a href="#" className="tick"></a>
+                          <img src="../../assets/images/visa.png" />
+                          <p>*8393</p>
+                        </Row>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col>
+                        <Row>
+                          <a href="#" className="tick">+</a>
+                          <p>Add Bank Account</p>
+                        </Row>
+                      </Col>
+                    </Row>
+                    <a href="#" className="button save-default nowrap">SAVE AS DEFAULT</a>
+                  </Col>
+              </Col>
+            </ContentContainer>
+          </Col>
+          <Clearfix />
         </SectionWithHeading>
       </Col>
     </Row>
