fc50d03789d62f6c2abcba3185bc5279dcf37848
Andrew Casal <andrewjcasal@gmail.com>
2018-04-13 16:28:07 -0700

    page progress

--- src/Pages/BusinessDetails.js
+++ src/Pages/BusinessDetails.js
@@ -26,6 +26,18 @@ class BusinessDetails extends React.Component {
     });
   }
 
+  handleChange = event => {
+    const {name, value} = event.target
+    this.setState({[name]: value})
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(`
+      organization: ${this.state.organization}
+    `)
+  }
+
   render() {
     return ( <Container>
       <Row>
@@ -34,38 +46,40 @@ class BusinessDetails extends React.Component {
         </Col>
         <Col md={9}>
           <SectionWithHeading heading="Business Details">
-            <ContentContainer type="Setup" left={
-              <div>
-                <Row>
-                  <Col md={12}><input type="text" placeholder="Organization Name" /></Col>
-                </Row>
-                <Row>
-                  <Col md={12}><input type="text" placeholder="YOURCLUB.cinchsports.com" /></Col>
-                </Row>
-                <Row>
-                  <Col md={12}><input type="text" placeholder="Address" /></Col>
-                </Row>
-                <Row>
-                  <Col md={4}><input type="text" placeholder="City" /></Col>
-                  <Col md={4}><Dropdown title="State" options={this.state.states}/></Col>
-                  <Col md={4}><input type="text" placeholder="Zip" /></Col>
-                </Row>
-                <Row>
-                  <Col md={4}><Dropdown title="Sport" options={this.state.sports}/></Col>
-                  <Col md={4}><Dropdown title="Affiliation" options={["male","female"]}/></Col>
-                  <Col md={4}><input type="text" placeholder="Federal Tax ID" /></Col>
-                </Row>
-                <Row>
-                  <Col md={12}><Wysiwyg /></Col>
-                </Row>
-              </div>
-            } right={
-              <DragFile label="Organization Logo" />
-            }>
-            </ContentContainer>
-            <Row>
-              <Col md={12}><FooterButtons centered save="black" /></Col>
-            </Row>
+            <form onSubmit={this.handleSubmit}>
+              <ContentContainer type="Setup" left={
+                <div>
+                  <Row>
+                    <Col md={12}><input type="text" name="organization" onChange={this.handleChange} placeholder="Organization Name" /></Col>
+                  </Row>
+                  <Row>
+                    <Col md={12}><input type="text" placeholder="YOURCLUB.cinchsports.com" /></Col>
+                  </Row>
+                  <Row>
+                    <Col md={12}><input type="text" placeholder="Address" /></Col>
+                  </Row>
+                  <Row>
+                    <Col md={4}><input type="text" placeholder="City" /></Col>
+                    <Col md={4}><Dropdown title="State" options={this.state.states}/></Col>
+                    <Col md={4}><input type="text" placeholder="Zip" /></Col>
+                  </Row>
+                  <Row>
+                    <Col md={4}><Dropdown title="Sport" options={this.state.sports}/></Col>
+                    <Col md={4}><Dropdown title="Affiliation" options={["male","female"]}/></Col>
+                    <Col md={4}><input type="text" placeholder="Federal Tax ID" /></Col>
+                  </Row>
+                  <Row>
+                    <Col md={12}><Wysiwyg /></Col>
+                  </Row>
+                </div>
+              } right={
+                <DragFile label="Organization Logo" />
+              }>
+              </ContentContainer>
+              <Row>
+                <Col md={12}><FooterButtons centered save="black" /></Col>
+              </Row>
+            </form>
           </SectionWithHeading>
         </Col>
       </Row>
